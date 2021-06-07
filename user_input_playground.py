db3ef98892089958ae68bcc37f6ae434dd57e359
jbehrend89 <j.behrend@hotmail.com>
2021-06-07 20:04:55 +0800

    Create user_input_playground.py
    Variable inputs

---
+++ user_input_playground.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+print(f"Hi {name}!")
+
+age = input(f"Hi {name}, how old are you?")
+years_until_100 = 100 - int(age)
+print(f"Wow {name}! You'll be 100 in {years_until_100}")
