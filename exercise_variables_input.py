18b13ffec47b193cf46a7030e14c4ab73c5bfe59
jbehrend89 <j.behrend@hotmail.com>
2021-06-07 20:31:49 +0800

    Create exercise_variables_input.py
    updates to variable exercises

---
+++ exercise_variables_input.py
@@ -0,0 +1,26 @@
+# addition exercise int
+addition1 = input("Choose a number between 1-9 ")
+addition2 = input("Choose another number between 1-9 ")
+
+total_value = int(addition1) + int(addition2)
+print(f"Adding those numbers together equals {total_value}")
+
+# addition exercise float
+addition1 = input("Choose a number between 1-9 ")
+addition2 = input("Choose another number between 1-9 ")
+
+total_value = int(addition1) + int(addition2)
+print(f"Adding those numbers together equals {total_value}")
+
+
+#multiplication exercise
+multiply1 = input ("Now choose a number between 1-9 ")
+multiply2 = input ("And another number between 1-9 ")
+
+multiply_value = int(multiply1) * int(multiply2)
+print(f"Wow I have multiplied those numbers for you and they equal {multiply_value}")
+
+#km to m
+km_dist = input ("How many kilometers did you run today? ")
+m_dist = int(km_dist) * 1000
+print(f"Great work! That's {m_dist} meters. Congratulations!")
