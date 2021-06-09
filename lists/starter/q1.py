9fb985f18655201c134757d718f26fd4c623df40
jbehrend89 <84775195+jbehrend89@users.noreply.github.com>
2021-06-09 19:15:38 +0800

    Updates
    Updates

---
+++ lists/starter/q1.py
@@ -0,0 +1,31 @@
+# question 1 
+foods = [
+    "orange",
+    "apple",
+    "banana",
+    "strawberry",
+    "grape",
+    "blueberry",
+    ["carrot", "cauliflower", "pumpkin"],
+    "passionfruit",
+    "mango",
+    "kiwifruit"
+]
+
+# find the first item in the list.
+print(foods[0])
+
+# the third item in the list.
+print(foods[2])
+
+# the last item in the list.
+print(foods[-1])
+
+# the first three items in the list.
+print(foods[0:3])
+
+# the last three items in the list.
+print(foods[-3:-1])
+
+# the last item in the sublist.
+print(foods[-1])
