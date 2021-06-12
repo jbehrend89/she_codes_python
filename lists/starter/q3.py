1734937eea48d0b91f5134e97ea79c15967bc276
jbehrend89 <84775195+jbehrend89@users.noreply.github.com>
2021-06-12 10:45:30 +0800

    updates to lists

---
+++ lists/starter/q3.py
@@ -0,0 +1,12 @@
+# Ask the user for three names, add them to a list, then print the list.
+names_list = []
+
+name1 = input("Enter a name.")
+name2 = input("Enter a name.")
+name3 = input("Enter a name.")
+
+names_list.append(name1)
+names_list.append(name2)
+names_list.append(name3)
+
+print(names_list)
